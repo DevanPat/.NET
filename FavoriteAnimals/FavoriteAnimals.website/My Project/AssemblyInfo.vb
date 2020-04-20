91b285f4431e65ad18f859bd996dc42f408bcf49
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-20 05:33:03 -0500

    changes

---
+++ FavoriteAnimals/FavoriteAnimals.website/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("FavoriteAnimals.website")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP Inc.")>
+<Assembly: AssemblyProduct("FavoriteAnimals.website")>
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("eb9ccd69-5d80-4dc6-8e7d-bb1f550e80ac")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Student's Daily Report/Student's Daily Report/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_s_Daily_Report
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("The Coursename is " + courseName);
+            Console.WriteLine("What page number are you on?");
+            int pageNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Page Number is " + pageNum);
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            bool doyouNeedHelp = false;
+            bool needHelp = true;
+            Console.WriteLine(doyouNeedHelp);
+            Console.WriteLine(needHelp);
+            Console.WriteLine("Were there any positive experiences you would like to share? Please give specifics.");
+            Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you would like to provide? Please be specific.");
+            Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("I have studied for " + hoursStudied + " hours.");
+            Console.WriteLine("Thankyou for all of your answers. An instructor will respond to you shortly! Have a great day!");
+            Console.ReadLine();
+
+            
+            
+
+        }
+    }
+}
