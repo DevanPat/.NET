7a5557938d9f57d590cc1a78754cd4e6d209f9a0
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-21 07:11:00 -0500

    committing changes

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1;
+            //Int32 num2 = 50;
+            //Console.WriteLine("Input first number to multiply ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //Int32 result = num1 * num2;
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Insert First Number Here");
+            //string num = Console.ReadLine();
+            //int num2 = Int32.Parse(num);
+            //int numb = num2 + 25;
+            //Console.WriteLine(numb);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Input Number Here");
+            //string num1 = Console.ReadLine();
+            //double num2 = Int32.Parse(num1);
+            //double number = num2 / 12.5;
+            //Console.WriteLine(number);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("Input your number here");
+            //string numb = Console.ReadLine();
+            //int num1 = Int32.Parse(numb);
+            //bool checkit = num1 > 50;
+            //Console.WriteLine(checkit);
+            //Console.ReadLine();
+
+            Console.WriteLine("Input your number here");
+            string number = Console.ReadLine();
+            int numb = Int32.Parse(number);
+            int num2 = numb / 9;
+            
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+          
+
+            
+           
+
+
+
+
+
+        }
+    }
+}
