04020dbd1e4bf8044fe27ac2d7c5965ddbfa6970
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-22 20:26:24 -0500

    committing changes

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool answer = Convert.ToBoolean(Console.ReadLine()); 
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool condition1 = age > 15;
+            bool condition2 = answer == false;
+            bool condition3 = tickets <= 3;
+            Console.WriteLine("See below to see if you qualified.");
+            if (condition1 == true && condition2 == true && condition3 == true)
+            {
+                Console.WriteLine(true);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(false);
+                Console.ReadLine();
+                    
+             }
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(false && false);
+            //Console.ReadLine();
+            //Console.WriteLine(true && false);
+            //Console.ReadLine();
+            //Console.WriteLine(true && true);
+            //Console.ReadLine();
+            //Console.WriteLine(false || false);
+            ////Console.ReadLine();
+            //Console.WriteLine(true || false);
+            //Console.ReadLine();
+            //Console.WriteLine(true || true);
+            //Console.ReadLine();
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+            //Console.ReadLine();
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ true);
+            //Console.WriteLine(false ^ false);
+            //Console.ReadLine();
+
+        }
+    }
+}
