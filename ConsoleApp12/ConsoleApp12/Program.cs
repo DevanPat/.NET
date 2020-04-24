4bbef7d26fddab6cf3900ccb6efed60340ebf737
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-24 07:35:58 -0500

    changes

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 10;
+            while (b < 15)
+            {
+                Console.WriteLine(b);
+                b++;
+            }
+            
+
+            int c = 25;
+            do
+            {
+                Console.WriteLine(c + 2);
+                c++;
+            }
+            while (c < 30);
+            Console.ReadLine();
+            
+
+            
+               
+        }
+    }
+}
