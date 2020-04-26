2ae8cd74715192573c39c026ac8e2af1a65e12af
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-26 01:38:18 -0500

    Committing changes

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Devan";
+            //string quote = "The man said, \\Hello\", Devan. \n Hello on a new line. \n \t Hello on a tab.";
+            //string filename = @"C:\\Users\\Devan";
+            //bool trueOrFalse = name.Contains("e");
+            //trueOrFalse = name.EndsWith("s");
+            //int length = name.Length;
+            //name = name.ToUpper();
+            //Console.WriteLine("Length");
+            //Console.WriteLine(quote);
+            //Console.WriteLine(filename);
+            //Console.WriteLine(length);
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Devan");
+
+            string name = "Devan";
+            name = "Jim";
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Greetings ";
+            string name2 = "and salutations from the Museum Hergenshmergelwerber's Planetarium";
+            string name3 = " I hope you enjoy the show.";
+            name1 = name1.ToUpper();
+            string name = name1 + name2 + name3;
+            Console.WriteLine(name);
+            StringBuilder Strinbu = new StringBuilder();
+            Strinbu.Append("My name is Devan Raj. ");
+            Strinbu.Append("I was born in Houston, Texas. ");
+            Strinbu.Append("I am 24 years old. ");
+            Strinbu.Append("I am half Indian.");
+            Console.WriteLine(Strinbu);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+            
+
+
