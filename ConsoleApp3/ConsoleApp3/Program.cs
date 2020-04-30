6eba5001c743b4073f53669b0bed6ad3bdff0546
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-30 06:41:58 -0500

    changes

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intLists = new List<int>();
+            intLists.Add(10);
+            intLists.Add(12);
+            intLists.Add(15);
+            intLists.Add(20);
+            Console.WriteLine("Enter a divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("The quotient is: " + intLists[i] / divisor);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppPg143/ConsoleAppPg143/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try
+        {
+            List<int> intLists = new List<int>();
+            intLists.Add(10);
+            intLists.Add(12);
+            intLists.Add(15);
+            intLists.Add(20);
+            Console.WriteLine("Enter a divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("The quotient is: " + intLists[i] / divisor);
+            }
+            Console.ReadLine();
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Type a whole number please.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("You can not divide by zero in this program.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
