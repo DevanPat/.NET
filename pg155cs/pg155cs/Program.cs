140025bb52f6007fb946b84adcdced1077fcf8dd
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-05-04 15:35:57 -0500

    changes

---
+++ pg155cs/pg155cs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg155cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methodC methodic1 = new methodC();
+            Console.WriteLine("User please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            
+            int answer1 = methodic1.methodOne(userInput);
+            Console.WriteLine("Your answer is " + answer1);
+            Console.ReadLine();
+
+            double answer2 = methodic1.methodTwo(userInput);
+            Console.WriteLine("Your answer is " + answer2);
+            Console.ReadLine();
+
+            Console.WriteLine("User please enter a number as a string.");
+            string userTypes = Console.ReadLine();
+            string answer3 = methodic1.methodThree(userTypes);
+            Console.WriteLine("Your answer is " + answer3);
+            Console.ReadLine();
+
+        }
+    }
+}
+            
+
+            
+
+
+
---
+++ pg155cs/pg155cs/methodC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg155cs
+{
+    class methodC
+    {
+        public int methodOne(int x)
+        {
+            return (x * 20);
+        }
+
+        public Double methodTwo(Double b)
+        {
+            return (b + 12.7);
+        }
+
+
+        public string methodThree(string a)
+        {
+            int i = Int32.Parse(a);
+            return (a + 15);
+
+        }
+    }
+}
+      
