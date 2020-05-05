85a847caed5f5d2658a61e22aecba2937b2da289
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-05-05 18:26:09 -0500

    committing changes

---
+++ ConsoleApppg157/ConsoleApppg157/ClassOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApppg157
+{
+    public class classOne
+    {
+        public void methodOne(int x, int y)
+        {
+            int result = x + 15;
+            Console.WriteLine(y);
+            
+        }
+    }
+}
---
+++ ConsoleApppg157/ConsoleApppg157/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApppg157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            classOne classOnes = new classOne();
+
+            Console.WriteLine("User please enter a number for a math operation");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number which will display on the screen");
+            int y = Convert.ToInt32(Console.ReadLine());
+            classOnes.methodOne(x, y);
+            Console.ReadLine();
+        }
+    }
+}
+
+             
+
