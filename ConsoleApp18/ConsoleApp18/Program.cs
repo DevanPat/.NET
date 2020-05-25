df0629f8ddb83c6f050a2f37cb7761856b3daf54
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-05-25 15:15:09 -0500

    committing changes

--- ConsoleApp18/ConsoleApp18/Program.cs
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -1,8 +1,11 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.IO;
 
-namespace ConsoleApppg210
+namespace ConsoleApp18
 {
     class Program
     {
@@ -11,9 +14,10 @@ namespace ConsoleApppg210
             Console.WriteLine("User please enter a number below.");
             string text = Console.ReadLine();
             File.WriteAllText(@"C:\Users\devan\c-logs\challenge1.txt", text);
-            File.ReadAllText(@"C:\Users\devan\c-logs\challenge1.txt");
-           
-           
+            Console.WriteLine(File.ReadAllText(@"C:\Users\devan\c-logs\challenge1.txt"));
+            Console.ReadLine();
+            
+
         }
     }
 }
