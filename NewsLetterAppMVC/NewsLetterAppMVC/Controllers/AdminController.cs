addfcad6a1f16d6754cc72c78fd05d9ca98f2235
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-07-29 19:54:04 -0500

    committing changes

---
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Routing.Constraints;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())
+            {
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
+                var SignUpVMs = new List<SignUpVM>();
+                foreach (var signup in signups)
+                {
+                    var SignUpVM = new SignUpVM();
+                    SignUpVM.Id = signup.Id;
+                    SignUpVM.FirstName = signup.FirstName;
+                    SignUpVM.LastName = signup.LastName;
+                    SignUpVM.EmailAddress = signup.EmailAddress;
+                    SignUpVMs.Add(SignUpVM);
+                }
+
+                return View(SignUpVMs);
+            }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities1 db = new NewsletterEntities1())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace NewsLetterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=LAPTOP-HVJORDUF\SQLEXPRESS;Initial Catalog = Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        
         public ActionResult Index()
         {
             return View();
@@ -27,62 +27,22 @@ namespace NewsLetterAppMVC.Controllers
             else
             {
                 
-                using (NewsletterEntities db = new NewsletterEntities())
+                using (NewsletterEntities1 db = new NewsletterEntities1())
                 {
                     var signup = new SignUp();
                     signup.FirstName = firstName;
                     signup.LastName = lastName;
                     signup.EmailAddress = emailAddress;
-
+                    
                     db.SignUps.Add(signup);
                     db.SaveChanges();
                 }    
 
-
-                //string queryString = @"INSERT INTO SignUps (FirstName, LastName, EmailAddress) VALUES
-                //                        (@FirstName, @LastName, @EmailAddress)";
-
-                //using (SqlConnection connection = new SqlConnection(connectionString))
-                //{
-                //    SqlCommand command = new SqlCommand(queryString, connection);
-                //    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                //    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                //    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
-
-                //    command.Parameters["@FirstName"].Value = firstName;
-                //    command.Parameters["@LastName"].Value = lastName;
-                //    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                //    connection.Open();
-                //    command.ExecuteNonQuery();
-                //    connection.Close();
-                        
-
-                //}
                     return View("Success");
             }
         }
 
-        public ActionResult Admin()
-        {
-            using (NewsletterEntities db = new NewsletterEntities())
-            {
-                var signups = db.SignUps;
-                var SignUpVMs = new List<SignUpVM>();
-                foreach (var signup in signups)
-                {
-                    var SignUpVM = new SignUpVM();
-                    SignUpVM.FirstName = signup.FirstName;
-                    SignUpVM.LastName = signup.LastName;
-                    SignUpVM.EmailAddress = signup.EmailAddress;
-                    SignUpVMs.Add(SignUpVM);
-                }
-
-                return View(SignUpVMs);
-            }
-
 
         }
 
-    }
 }
